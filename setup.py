c4a23001ce0afd1343d71f9a94f7c3b02e0bb89c
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2022-06-20 10:42:06 +0100

    increased version and removed simple-workflow

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='petrocks',
-    version='0.3.1',    
+    version='0.3.2',    
     description='Package for making pet rocks',
     url='https://github.com/srh-sloan/pet-rocks',
     author='Sarah Sloan',
